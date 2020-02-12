b51cc33399854ae484c8f599c2b8fbbe8083a8d5
ktabusa laptop <ktabusa@gmail.com>
2020-02-11 16:15:54 -0800

    First update with first python docs: 1. Intro / Integers (done) 2. Integers (done) 3. Lists / Sets (wip)

---
+++ ListSet.py
@@ -0,0 +1,13 @@
+courses = ['History', 'Math', 'Physics', 'CompSci']
+
+print(courses)
+print(len(courses))
+
+print(courses[2])
+# starting point in a list is index 0
+
+print(courses[-1])
+# can search for the last item, etc.
+
+print(courses)
+print(courses[2:])
---
+++ integers.py
@@ -0,0 +1,95 @@
+num = 3
+
+print(type(num))
+
+
+num = 3.14
+
+print(type(num))
+
+# Arithmetic Operators:
+# Addition:                 3 + 2
+# Subtraction:             3 - 2
+# Multiplication:        3 * 2
+# Division:                 3 / 2
+# Floor Division:       3 / / 2
+# Exponent:               3 * * 2
+# Modulus:                3 % 2
+
+print(3/2)
+
+print(3 // 2)
+# in Python 2 decimals are dropped
+# Floor division does the same
+
+print(3**2)
+message = ' '
+print(message)
+print(2 % 2)
+print(3 % 2)
+
+# called a modular operator, only provides the remainder
+
+print(17 % 2.3)
+
+# can be used to determine if a number is even or odd
+
+print(3 * 2 + 1)
+print(3 * (2 + 1))
+# can use parens in arithmetic Operators
+
+print(message)
+num = 1
+num = num + 1
+
+print(num)
+
+num += 1
+
+print(num)
+print(num)
+# note that this doesn't repeat unless the num note is repeated
+
+print(message)
+num *= 10
+
+print(num)
+
+print(message)
+
+print(abs(-3))
+
+print(message)
+print(message)
+print(round(3.745))
+# by default this rounds to the nearest integet
+
+print(message)
+print(round(3.745, 2))
+
+# Comparisons:
+# Equal:                    3 == 2
+# Not Equal              3 != 2
+# Greater Than:        3 > 2
+# Less Than:             3 < 2
+# Greater or Equal:  3 >= 2
+# Less or Equal:        3 <= 2
+
+num_1 = 3
+num_2 = 2
+
+print(num_1 < num_2)
+print(num_1 != num_2)
+
+print(message)
+
+num_1 = '100'
+num_2 = '200'
+
+print(num_1 + num_2)
+# this concatentates the numbers, but in order to cast them from a string to an integer we need to perform casting
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
