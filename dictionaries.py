a5e01cd9c54444080bf1ed7017bafd1b76c1076d
ktabusa <ktabusa@gmail.com>
2020-02-13 14:25:41 -0800

    updated desktop settings for atom/git

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+
+# Dictionaries
+# aka key value pairs, hashmaps, or associative arrays
+# creating a student dictionary
+
+student = ['name': 'John, 'age': 25, 'courses': ['Math', 'CompSci']
+
+print(student['courses'])
+
+
+print(student['courses
